7b4129ad14442464c1e2dffc849e3b4c016279e5
Hugo Hirsch <github@hugo-hirsch.de>
2017-01-12 10:31:18 +0100

    Make form dynamic (nneds to be put into the PHP part)

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#room").change(
+        function() {
+            if (this.selectedIndex === 0) {
+                $("#together2-group").hide();
+            }
+            else {
+                $("#together2-group").show();
+            }
+        });
+    }
+);
