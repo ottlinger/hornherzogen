090e98f7880581ac90420233421769daa5e62202
Hugo Hirsch <github@hugo-hirsch.de>
2017-01-15 20:41:38 +0100

    Add registration target mail.

--- inc/config.template.php
+++ inc/config.template.php
@@ -1,5 +1,9 @@
 <?php
 // Please rename this file to config.php in your live setup and adapt all values properly
+// contact mail
 $cfg_mail = 'foo@bar.de';
-$cfg_pdf = 'any.pdf';
+// location of the seminar announcement
+$cfg_pdf = 'https://example.com/data/any.pdf';
+// where to send registration data to
+$cfg_registrationmail = 'foo@buzz.de';
 ?>
