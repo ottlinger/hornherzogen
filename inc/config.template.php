441afd37162318ab76b0635b9b344a443b199894
Hugo Hirsch <github@hugo-hirsch.de>
2017-01-21 22:52:36 +0100

    Add error stacktraces in config

--- inc/config.template.php
+++ inc/config.template.php
@@ -1,7 +1,7 @@
 <?php
 // allow debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
 error_reporting(E_ALL);
 
 // Please rename this file to config.php in your live setup and adapt all values properly
