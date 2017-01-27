620798736e784cb0410c66d3bab6cbd7b18d532e
Hugo Hirsch <github@hugo-hirsch.de>
2017-01-27 22:14:53 +0100

    Make configuration init aware of missing file and fallback to template.

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,26 @@
 <?php
-$GLOBALS["horncfg"] = parse_ini_file("config.ini.php");
 
-if ($GLOBALS["horncfg"]["debug"]) {
+// Check whether the file exists or fallback to the template
+if (file_exists('config.ini.php')) {
+    $filename = 'config.ini.php';
+} else {
+    $filename = 'config.ini.php.template';
+}
+$isTemplate = strpos($filename, '.template') !== FALSE;
+
+// load configuration and dump if template or debug=true
+$GLOBALS["horncfg"] = parse_ini_file('config.ini.php');
+if ($GLOBALS["horncfg"]["debug"] || $isTemplate) {
     // allow error reporting
     ini_set('display_errors', 'on');
     ini_set('display_startup_errors', 'on');
     error_reporting(E_ALL);
 
     echo "<pre>";
+    if ($isTemplate) {
+        echo "<strong>You should adapt your configuration and save it as config.ini.php, currently the dummy template is used.</strong>";
+    }
+
     print_r($GLOBALS["horncfg"]);
     echo "</pre>";
 }
