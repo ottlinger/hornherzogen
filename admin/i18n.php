f464dc217bc5c769c387e26eb8a825c16bd4211d
Hugo Hirsch <github@hugo-hirsch.de>
2017-01-29 22:47:30 +0100

    Add i18n example (needs a module) Fix admin area.

--- admin/i18n.php
+++ admin/i18n.php
@@ -1,5 +1,14 @@
 <?php
+require '../vendor/autoload.php';
 
 echo "<p>Trying to check that localization is possible</p>";
 
+$messages = array();
+$messages['de_DE'] = array('KEY_WITH_PARAM' => 'Meine besonderen Interessen liegen im Bereich {0} uvm.',
+    'KEY_PLAIN' => 'Ohne Parameter kann doch wohl jeder');
+
+$withParam = new MessageFormatter('de_DE', $messages['de_DE']['KEY_WITH_PARAM']);
+$withoutParam = new MessageFormatter('de_DE', $messages['de_DE']['KEY_PLAIN']);
+
+echo "<h1>Mit Parameter: ".$withParam->format(array("PHP und OpenSource, Tendoryu Aikido"))."</h1>";
 
--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
-<?xml version="1.0"?>
+<?php echo '<?xml version="1.0"?>'; ?>
 <html>
   <body>
-    <h1>Welcome authenticated test user :-)</h1>
+    <h1>Welcome to the administrative area of project hornherzogen</h1>
     <?php echo "<p>Hello, your username is '{$_SERVER['PHP_AUTH_USER']}'.</p>"; ?>
   </body>
 </html>
