17129dbc9a34ab4890271f9941d36940a61cd1bd
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-03 00:03:43 +0100

    Prepare Persistenzierungsschicht

---
+++ src/hornherzogen/DatabaseWriter.php
@@ -0,0 +1,14 @@
+<?php
+namespace hornherzogen;
+
+
+class DatabaseWriter
+{
+    // contains the DB access and helper methods e.g.
+    // 1)exists(firstname, lastname) to properly calculate the combinedName as firstname + timestamp + lastname to avoid disambiguities
+    // 2)persist()
+    // 3)getByName(....) - makes a search in combinedName column
+    
+
+
+}
