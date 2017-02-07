cd414f360fa4d04b039d9b7c0a9841dce0256dec
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-07 12:13:05 +0100

    Raise test coverage by manually including the inc-files

---
+++ tests/IncludeFilesTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+// Included automatically via phpunit.xml
+//include_once 'inc/config.php';
+//include_once 'inc/session.php';
+//include_once 'inc/localization.php';
+
+class IncludeFilesTest extends TestCase
+{
+    public function testConfigurationHasBeenRead()
+    {
+        $this->assertNotEmpty($GLOBALS["horncfg"]);
+    }
+
+    public function testLocalizationIsInitialized()
+    {
+        $this->assertNotEmpty($GLOBALS['messages']);
+    }
+
+    public function testSessionIsStarted()
+    {
+        $this->assertEquals(PHP_SESSION_ACTIVE, session_status());
+    }
+
+}
