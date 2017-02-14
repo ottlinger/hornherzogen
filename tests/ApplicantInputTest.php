3ed662681202bf1d489d4d18c7cf05610983de2b
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-14 22:24:23 +0100

    Issue #18: add grading since.

--- tests/ApplicantInputTest.php
+++ tests/ApplicantInputTest.php
@@ -348,4 +348,21 @@ class ApplicantInputTest extends TestCase
         $this->assertNotEmpty($this->applicantInput->showIsSuccess($field));
     }
 
+    public function testErroneousParsingOfFieldGradingSince()
+    {
+        $field = "gsince";
+        self::prepareForErrorParsing($field);
+        $this->applicantInput->parse();
+        $this->assertNotEmpty($this->applicantInput->showHasError($field));
+    }
+
+    public function testSuccessfulParsingOfFieldGradingSince()
+    {
+        $field = "gsince";
+        self::prepareForSuccessfulParsing($field);
+        $this->applicantInput->parse();
+        $this->assertEquals('', $this->applicantInput->showHasError($field));
+        $this->assertNotEmpty($this->applicantInput->showIsSuccess($field));
+    }
+
 }
