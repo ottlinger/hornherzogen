60f9dee04a733afe4c9d9ce901ee817fc2d82779
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-20 23:37:59 +0100

    Issue #2: Add newly introduced constraints to admin area

--- admin/dbtest.php
+++ admin/dbtest.php
@@ -11,18 +11,18 @@ if (ConfigurationWrapper::isValidDatabaseConfig()) {
         $db = new PDO('mysql:host=' . ConfigurationWrapper::dbhost() . ';dbname=' . ConfigurationWrapper::dbname(), ConfigurationWrapper::dbuser(), ConfigurationWrapper::dbpassword());
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $result = $db->exec("INSERT INTO applicants (vorname,nachname) VALUES ('Hugo','Hirsch')");
+        $result = $db->exec("INSERT INTO `applicants` (vorname,nachname,statusId) SELECT 'Hugo','Hirsch',id from `status` WHERE NAME = 'APPLIED'");
         if (false === $result) {
             $error = $db->errorInfo();
             print "DB-Error\nSQLError=$error[0]\nDBError=$error[1]\nMessage=$error[2]";
         }
 
-        $q = $db->query("SELECT vorname,nachname,created from applicants");
+        $q = $db->query("SELECT a.vorname, a.nachname, s.name FROM `applicants` a, `status` s WHERE s.id = a.statusId");
         while ($row = $q->fetch()) {
-            print "<h2>$row[vorname] $row[nachname] created at $row[created]</h2>\n";
+            print "<h2>$row[vorname] $row[nachname] created at $row[created] with status $row[name]</h2>\n";
         }
 
-        $result = $db->exec("DELETE from applicants where vorname='Hugo'");
+        $result = $db->exec("DELETE FROM `applicants` WHERE vorname='Hugo'");
 
     } catch (PDOException $e) {
         print "Unable to connect to db:" . $e->getMessage();
