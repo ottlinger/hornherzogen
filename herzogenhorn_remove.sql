5bd559ad1855ffe7f56d1f94db62af58ac0759c0
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-22 01:09:56 +0100

    Issue #1: Provide easy way to delete data from database

---
+++ herzogenhorn_remove.sql
@@ -0,0 +1,6 @@
+-- Use these commands to *completely* wipe the database
+-- and import herzogenhorn.sql afterwards
+DROP TABLE roombooking;
+DROP TABLE applicants;
+DROP TABLE status;
+DROP TABLE rooms;
