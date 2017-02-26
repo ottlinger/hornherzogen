21461a7dd0e9af1a54d891dc307aa95fd2e2fe80
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-26 20:59:14 +0100

    Issue #24: Mask sensitive configuration data

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,7 @@ use hornherzogen\ConfigurationWrapper;
 
 // Check whether the file exists or fallback to the template
 // DEVHINT: it's quite odd that file_exists seems to start at root, while parse takes the relative path from this file
-if (file_exists(dirname(__FILE__).'/config.ini.php')) {
+if (file_exists(dirname(__FILE__) . '/config.ini.php')) {
     $filename = 'config.ini.php';
 } else {
     $filename = 'config.ini.php.template';
@@ -23,11 +23,10 @@ if ($config->debug() || $isTemplate) {
 
     echo "<pre>";
     if ($isTemplate) {
-        echo "<strong>You should adapt your configuration and save it as config.ini.php, currently the dummy template is used.</strong>";
+        echo "<strong>You should adapt your configuration and save it as config.ini.php, currently the dummy template is used, which means NO database connection!</strong>";
+    } else {
+        echo $config;
     }
-
-    // TODO #24: replace all passwords with asterisk
-    print_r($GLOBALS["horncfg"]);
     echo "</pre>";
 }
 
--- src/hornherzogen/ConfigurationWrapper.php
+++ src/hornherzogen/ConfigurationWrapper.php
@@ -1,22 +1,59 @@
 <?php
 namespace hornherzogen;
 
-
 /**
  * Class ConfigurationWrapper wraps the configuration of the application loaded from the INI file.
  * @package hornherzogen
  */
 class ConfigurationWrapper
 {
-    const LINEBREAK = "<br />\r\n";
+    const LINEBREAK = "<br/>";
+    private $formHelper;
+
+    public function __construct()
+    {
+        $this->formHelper = new FormHelper();
+    }
 
     public function __toString()
     {
         $status = "<pre>Current configuration is: " . self::LINEBREAK;
-        $status .= "DB-User: " . self::maskWithAsterisk($this->dbuser(), 3) . self::LINEBREAK;
+        $status .= "Debug mode: " . (boolval($this->debug()) ? "ON" : "OFF") . self::LINEBREAK;
+        $status .= "Send mails: " . (boolval($this->sendregistrationmails()) ? "ON" : "OFF") . self::LINEBREAK;
+        $status .= "Send mails internally: " . (boolval($this->sendinternalregistrationmails()) ? "ON" : "OFF") . self::LINEBREAK;
+        $status .= "Contact Email: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->mail()), 7) . self::LINEBREAK;
+        $status .= "Registration Email: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->registrationmail()), 10) . self::LINEBREAK;
+        $status .= "DB-Host: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->dbhost()), 3) . self::LINEBREAK;
+        $status .= "DB-Name: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->dbname()), 3) . self::LINEBREAK;
+        $status .= "DB-User: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->dbuser()), 3) . self::LINEBREAK;
+        $status .= "DB-PW: " . self::maskWithAsterisk($this->formHelper->filterUserInput($this->dbpassword()), 1) . self::LINEBREAK;
+        $status .= "Application PDF: " . $this->formHelper->filterUserInput($this->pdf()) . self::LINEBREAK;
         $status .= "</pre>";
 
         return $status;
+    }
+
+    public function debug()
+    {
+        return self::getFromHornConfiguration('debug');
+    }
+
+    private static function getFromHornConfiguration($key)
+    {
+        if ($GLOBALS['horncfg'] && isset($GLOBALS['horncfg'][$key])) {
+            return trim($GLOBALS['horncfg'][$key]);
+        }
+        return NULL;
+    }
+
+    public function sendregistrationmails()
+    {
+        return boolval(self::getFromHornConfiguration('sendregistrationmails'));
+    }
+
+    public function sendinternalregistrationmails()
+    {
+        return boolval(self::getFromHornConfiguration('sendinternalregistrationmails'));
     }
 
     static function maskWithAsterisk($input, $fromPosition = 0)
@@ -37,61 +74,14 @@ class ConfigurationWrapper
         return $input;
     }
 
-    public function dbuser()
-    {
-        return self::getFromHornConfiguration('dbuser');
-    }
-
-    private static function getFromHornConfiguration($key)
-    {
-        if ($GLOBALS['horncfg'] && isset($GLOBALS['horncfg'][$key])) {
-            return trim($GLOBALS['horncfg'][$key]);
-        }
-        return NULL;
-    }
-
     public function mail()
     {
         return self::getFromHornConfiguration('mail');
     }
 
-    public function debug()
-    {
-        return self::getFromHornConfiguration('debug');
-    }
-
-    public function pdf()
-    {
-        return self::getFromHornConfiguration('pdf');
-    }
-
     public function registrationmail()
     {
         return self::getFromHornConfiguration('registrationmail');
-    }
-
-    public function sendregistrationmails()
-    {
-        return boolval(self::getFromHornConfiguration('sendregistrationmails'));
-    }
-
-    public function sendinternalregistrationmails()
-    {
-        return boolval(self::getFromHornConfiguration('sendinternalregistrationmails'));
-    }
-
-    /**
-     * @return bool TRUE if database configuration is complete
-     */
-
-    public function isValidDatabaseConfig()
-    {
-        return !empty($this->dbpassword()) && !empty($this->dbuser()) && !empty($this->dbhost()) && !empty($this->dbname());
-    }
-
-    public function dbpassword()
-    {
-        return self::getFromHornConfiguration('dbpassword');
     }
 
     public function dbhost()
@@ -102,6 +92,30 @@ class ConfigurationWrapper
     public function dbname()
     {
         return self::getFromHornConfiguration('dbname');
+    }
+
+    public function dbuser()
+    {
+        return self::getFromHornConfiguration('dbuser');
+    }
+
+    public function dbpassword()
+    {
+        return self::getFromHornConfiguration('dbpassword');
+    }
+
+    public function pdf()
+    {
+        return self::getFromHornConfiguration('pdf');
+    }
+
+    /**
+     * @return bool TRUE if database configuration is complete
+     */
+
+    public function isValidDatabaseConfig()
+    {
+        return !empty($this->dbpassword()) && !empty($this->dbuser()) && !empty($this->dbhost()) && !empty($this->dbname());
     }
 
 }
