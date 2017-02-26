cc710cf879334acba5fd602d601c13a4ffc210e6
Hugo Hirsch <github@hugo-hirsch.de>
2017-02-26 21:07:28 +0100

    Issue #24: reformat admin area with configuration

--- admin/index.php
+++ admin/index.php
@@ -17,34 +17,33 @@ echo '<?xml version="1.0"?>';
 <h1>Welcome to the administrative area of project hornherzogen</h1>
 <?php
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    echo "<p>Hello, unable to extract your username :-(.</p>";
+    echo "<p>Hi unknown :-(. This is okay if working locally since HttpBasicAuth is not enabled for localhost.</p>";
 } else {
     echo "<p>Hello, your username is '{$_SERVER['PHP_AUTH_USER']}'.</p>";
 }
 ?>
-<h2>Possible setup/verification actions</h2>
+<h2>Possible admin actions</h2>
 <ul>
-    <li><a href="db/db_connect.php">ensure <strong>database</strong> can be connected to</a></li>
-    <li><a href="db/dbtest.php">ensure <strong>database tables</strong> are working smoothly, performs a minor roundtrip based on some assumptions</a></li>
     <li><a href="db/db_rooms.php">show list of <strong>available rooms</strong> in the database</a></li>
     <li><a href="db/db_applicants.php">show list of <strong>applications</strong> and their current state</a></li>
     <li><a href="db/db_bookings.php">show list of <strong>room bookings</strong> in the database</a></li>
+    <li><a href="..">return to <strong>main application</strong></a></li>
+</ul>
+<h2>One-time setup and verification actions</h2>
+<ul>
     <li><a href="i18n.php">ensure <strong>localization</strong> is working smoothly</a></li>
     <li><a href="path.php">show <strong>path to authentication setup</strong>, which needs to be changed manually after
             deployment to prod</a></li>
     <li><a href="php.php">show <strong>php</strong> version and extension settings</a></li>
+    <li><a href="showconfig.php">show <strong>application configuration</strong> of this app</a></li>
+    <li><a href="db/dbtest.php">ensure <strong>database tables</strong> are working smoothly, performs a minor roundtrip based on some assumptions</a></li>
     <li><a href="..">return to <strong>main application</strong></a></li>
 </ul>
 <h2>Possible admin actions - tbd</h2>
 <ul>
     <li>perform bookings (set room number, match wanted partners)</li>
     <li>change state of application to trigger final confirmation mails</li>
+    <li><a href="..">return to <strong>main application</strong></a></li>
 </ul>
-<h2>What's the configuration like?</h2>
-<p>
-    <?php
-        echo new ConfigurationWrapper();
-    ?>
-</p>
 </body>
 </html>
---
+++ admin/showconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use \hornherzogen\ConfigurationWrapper;
+
+echo "<h2>What's the current configuration?</h2>";
+echo new ConfigurationWrapper();
