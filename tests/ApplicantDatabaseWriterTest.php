21418947be6d11908d8def4ed342398005ecad13
Hugo Hirsch <github@hugo-hirsch.de>
2017-03-05 22:07:27 +0100

    Issue #35: Fix test setup for subclass.

--- tests/ApplicantDatabaseWriterTest.php
+++ tests/ApplicantDatabaseWriterTest.php
@@ -1,7 +1,36 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use hornherzogen\db\ApplicantDatabaseWriter;
+
 
 class ApplicantDatabaseWriterTest extends TestCase
 {
+    private $writer = null;
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $this->writer = new ApplicantDatabaseWriter();
+    }
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        $this->writer = null;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('hornherzogen\db\ApplicantDatabaseWriter', $this->writer);
+    }
 
 }
