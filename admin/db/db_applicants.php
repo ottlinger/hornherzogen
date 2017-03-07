8af50fbdfd8992a69019dfa25629adc097049929
Hugo Hirsch <github@hugo-hirsch.de>
2017-03-07 23:58:06 +0100

    Issue #50: introduce bootsrtap in admin area

--- admin/db/db_applicants.php
+++ admin/db/db_applicants.php
@@ -1,128 +1,262 @@
+<!DOCTYPE html>
 <?php
 require '../../vendor/autoload.php';
 
 use hornherzogen\ConfigurationWrapper;
 use hornherzogen\db\ApplicantDatabaseWriter;
 use hornherzogen\FormHelper;
+use hornherzogen\HornLocalizer;
 
-echo "<h1>List all applicants ....</h1>";
+?>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Herzogenhorn 2017 Anmeldung">
+    <meta name="author" content="OTG">
+    <meta name="robots" content="none,noarchive,nosnippet,noimageindex"/>
+    <link rel="icon" href="../../favicon.ico">
 
-$config = new ConfigurationWrapper();
+    <title>Herzogenhorn Anmeldeformular</title>
 
-if ($config->isValidDatabaseConfig()) {
+    <!-- Bootstrap core CSS -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../../css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    try {
-        $db = new PDO('mysql:host=' . $config->dbhost() . ';dbname=' . $config->dbname(), $config->dbuser(), $config->dbpassword());
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <!-- Custom styles for this template -->
+    <link href="../../css/starter-template.css" rel="stylesheet">
+    <link href="../../css/theme.css" rel="stylesheet">
 
-        // STATS
-        echo "<h3>Applicants by week and status</h3>";
-        $q = $db->query("SELECT s.name, a.week, a.statusId, count(*) AS howmany from `status` s, `applicants` a WHERE a.statusId=s.id GROUP BY a.statusId, a.week ORDER BY a.week");
-        if (false === $q) {
-            $error = $db->errorInfo();
-            print "DB-Error\nSQLError=$error[0]\nDBError=$error[1]\nMessage=$error[2]";
-        }
-        $rowNum = 0;
-        while ($row = $q->fetch()) {
-            $rowNum++;
-            print "<h2>($rowNum) '$row[name]' in week $row[week] for $row[howmany] applicants</h2>\n";
-        }
+    <!-- Calendar-related stuff -->
+    <link href="../../css/bootstrap-formhelpers.min.css" rel="stylesheet">
 
-        /*
-                // ALL with status
-                echo "<h2>Show all applicants and their status ...</h2>";
-                $q = $db->query("SELECT a.vorname, a.nachname, a.created, s.name FROM `applicants` a, `status` s WHERE s.id = a.statusId ORDER BY s.name");
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../../index.php"><span class="glyphicon glyphicon-tree-conifer"></span>
+                Adminbereich Herzogenhorn 2017</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <!-- TODO FIXME menu structure -->
+                <li class="active"><a href="../../admin" target="_blank"><span
+                                class="glyphicon glyphicon-briefcase"></span> <?php echo HornLocalizer::i18n('MENU.ADMIN'); ?>
+                    </a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+</nav>
+
+<div class="container theme-showcase">
+    <div class="starter-template">
+        <a href="https://github.com/ottlinger/hornherzogen" target="_blank"><img
+                    style="position: absolute; top: 100px; right: 0; border: 0;"
+                    src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67"
+                    alt="Fork me on GitHub"
+                    data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+
+        <h1>
+            <span class="glyphicon glyphicon-sunglasses"></span> <?php echo HornLocalizer::i18n('FORM.TITLE'); ?>
+        </h1>
+
+        <p>
+            <?php
+            echo "<h2>List all applicants ....</h2>";
+
+            $config = new ConfigurationWrapper();
+
+            if ($config->isValidDatabaseConfig()) {
+
+            try {
+                $db = new PDO('mysql:host=' . $config->dbhost() . ';dbname=' . $config->dbname(), $config->dbuser(), $config->dbpassword());
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // STATS
+                echo "<p>Applicants by week and status</p>";
+                $q = $db->query("SELECT s.name, a.week, a.statusId, count(*) AS howmany from `status` s, `applicants` a WHERE a.statusId=s.id GROUP BY a.statusId, a.week ORDER BY a.week");
                 if (false === $q) {
                     $error = $db->errorInfo();
                     print "DB-Error\nSQLError=$error[0]\nDBError=$error[1]\nMessage=$error[2]";
                 }
-
-                echo "<h1>Currently there are " . $q->rowCount() . " applicants in the database</h1>";
-
                 $rowNum = 0;
                 while ($row = $q->fetch()) {
                     $rowNum++;
-                    print "<h2>($rowNum) $row[vorname] $row[nachname] created at $row[created] with status $row[name]</h2>\n";
+                    print "<h2>($rowNum) '$row[name]' in week $row[week] for $row[howmany] applicants</h2>\n";
                 }
-        */
 
-    } catch (PDOException $e) {
-        print "Unable to connect to db:" . $e->getMessage();
-    }
+                /*
+                        // ALL with status
+                        echo "<h2>Show all applicants and their status ...</h2>";
+                        $q = $db->query("SELECT a.vorname, a.nachname, a.created, s.name FROM `applicants` a, `status` s WHERE s.id = a.statusId ORDER BY s.name");
+                        if (false === $q) {
+                            $error = $db->errorInfo();
+                            print "DB-Error\nSQLError=$error[0]\nDBError=$error[1]\nMessage=$error[2]";
+                        }
 
-    // retrieve via helper
-    print "<h1>So wird es zukünftig mal aussehen</h1>";
+                        echo "<h1>Currently there are " . $q->rowCount() . " applicants in the database</h1>";
 
-    ?>
-    <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                        $rowNum = 0;
+                        while ($row = $q->fetch()) {
+                            $rowNum++;
+                            print "<h2>($rowNum) $row[vorname] $row[nachname] created at $row[created] with status $row[name]</h2>\n";
+                        }
+                */
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label" for="week">Welche Woche zeigen?</label>
-            <?php
-            // filter for week?
-            $formHelper = new FormHelper();
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $week = $formHelper->filterUserInput($_POST['week']);
-                echo strlen($week) ? "Filterung nach Woche " . $week : "beide";
+            } catch (PDOException $e) {
+                print "Unable to connect to db:" . $e->getMessage();
             }
+
+            // retrieve via helper
+            print "<h1>So wird es zukünftig mal aussehen</h1>";
+
             ?>
-            <div class="col-sm-10">
-                <select class="form-control" id="week" name="week" onchange="this.form.submit()">
-                    <option value="">beide</option>
-                    <option value="1" <?php if (isset($week) && 1 == $week) echo ' selected'; ?>>1.Woche</option>
-                    <option value="2" <?php if (isset($week) && 2 == $week) echo ' selected'; ?>>2.Woche</option>
-                </select>
+        <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="week">Welche Woche zeigen?
+                    <?php
+                    // filter for week?
+                    $formHelper = new FormHelper();
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $week = $formHelper->filterUserInput($_POST['week']);
+                        echo strlen($week) ? "(aktiv Woche " . $week . ")" : "";
+                    }
+                    ?>
+                </label>
+                <div class="col-sm-10">
+                    <select class="form-control" id="week" name="week" onchange="this.form.submit()">
+                        <option value="">beide</option>
+                        <option value="1" <?php if (isset($week) && 1 == $week) echo ' selected'; ?>>1.Woche
+                        </option>
+                        <option value="2" <?php if (isset($week) && 2 == $week) echo ' selected'; ?>>2.Woche
+                        </option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <noscript><input type="submit" value="submit"></noscript>
-    </form>
+            <noscript>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default btn-primary" title="Submit">Submit</button>
+                    </div>
+                </div>
+            </noscript>
+        </form>
 
-    <?php
+        <?php
 
-    $writer = new ApplicantDatabaseWriter();
-    $applicants = $writer->getAllByWeek($week);
-    echo "<table>";
-    echo "<thead>";
-    echo "<tr>";
-    echo "<td>DB-Id</td>";
-    echo "<td>Woche</td>";
-    echo "<td>Anrede</td>";
-    echo "<td>Vorname</td>";
-    echo "<td>Nachname</td>";
-    echo "<td>Gesamtname</td>";
-    echo "<td>Adresse</td>";
-    echo "<td>PLZ/Stadt</td>";
-    echo "<td>Land</td>";
-    echo "<td>E-Mail</td>";
-    echo "<td>Dojo</td>";
-    echo "<td>Graduierung</td>";
-    echo "<td>twa?</td>";
-    echo "<td>Zimmer</td>";
-    echo "</tr>";
-    echo "</thead>";
-    echo "<tbody>";
-    foreach ($applicants as $applicant) {
+        $writer = new ApplicantDatabaseWriter();
+        $applicants = $writer->getAllByWeek($week);
+        echo '<div class="table-responsive"><table class="table table-striped">';
+        echo "<thead>";
         echo "<tr>";
-        echo "<td>" . $applicant->getPersistenceId() . "</td>";
-        echo "<td>" . $applicant->getWeek() . "</td>";
-        echo "<td>" . $applicant->getGender() . "</td>";
-        echo "<td>" . $applicant->getFirstname() . "</td>";
-        echo "<td>" . $applicant->getLastname() . "</td>";
-        echo "<td>" . $applicant->getFullName() . "</td>";
-        echo "<td>" . $applicant->getStreet() . " " . $applicant->getHouseNumber() . "</td>";
-        echo "<td>" . $applicant->getZipCode() . " " . $applicant->getCity() . "</td>";
-        echo "<td>" . $applicant->getCountry() . "</td>";
-        echo "<td>" . $applicant->getEmail() . "</td>";
-        echo "<td>" . $applicant->getDojo() . "</td>";
-        echo "<td>" . $applicant->getGrading() . " seit " . $applicant->getDateOfLastGrading() . "</td>";
-        echo "<td>" . (strlen($applicant->getTwaNumber()) ? " ja,  " . $applicant->getTwaNumber() : "nein") . "</td>";
-        echo "<td>" . $applicant->getRoom() . "</td>";
+        echo "<th>DB-Id</th>";
+        echo "<th>Woche</th>";
+        echo "<th>Anrede</th>";
+        echo "<th>Vorname</th>";
+        echo "<th>Nachname</th>";
+        echo "<th>Gesamtname</th>";
+        echo "<th>Adresse</th>";
+        echo "<th>PLZ/Stadt</th>";
+        echo "<th>Land</th>";
+        echo "<th>E-Mail</th>";
+        echo "<th>Dojo</th>";
+        echo "<th>Graduierung</th>";
+        echo "<th>twa?</th>";
+        echo "<th>Zimmer</th>";
+        echo "<th>Zusammenlegungswunsch</th>";
+        echo "<th>Essen</th>";
+        echo "<th>Umbuchbar?</th>";
+        echo "<th>Anmerkungen</th>";
+        echo "<th>aktueller Status</th>";
+        echo "<th>Statusübersicht</th>";
         echo "</tr>";
-    }
-    echo "</tbody>";
-    echo "</table>";
+        echo "</thead>";
+        echo "<tbody>";
+        foreach ($applicants as $applicant) {
+            echo "<tr>";
+            echo "<td>" . $applicant->getPersistenceId() . "</td>";
+            echo "<td>" . $applicant->getWeek() . "</td>";
+            echo "<td>" . $applicant->getGender() . "</td>";
+            echo "<td>" . $applicant->getFirstname() . "</td>";
+            echo "<td>" . $applicant->getLastname() . "</td>";
+            echo "<td>" . $applicant->getFullName() . "</td>";
+            echo "<td>" . $applicant->getStreet() . " " . $applicant->getHouseNumber() . "</td>";
+            echo "<td>" . $applicant->getZipCode() . " " . $applicant->getCity() . "</td>";
+            echo "<td>" . $applicant->getCountry() . "</td>";
+            echo "<td>" . $applicant->getEmail() . "</td>";
+            echo "<td>" . $applicant->getDojo() . "</td>";
+            echo "<td>" . $applicant->getGrading() . " seit " . $applicant->getDateOfLastGrading() . "</td>";
+            echo "<td>" . (strlen($applicant->getTwaNumber()) ? " ja,  " . $applicant->getTwaNumber() : "nein") . "</td>";
+            echo "<td>" . $applicant->getRoom() . "</td>";
+            echo "<td>" . (strlen($applicant->getPartnerOne()) || strlen($applicant->getPartnerTwo()) ? $applicant->getPartnerOne() . " " . $applicant->getPartnerTwo() : "keiner") . "</td>";
+            echo "<td>" . $applicant->getFoodCategory() . "</td>";
+            echo "<td>" . ($applicant->getFlexible() ? "ja" : "nein") . "</td>";
+            echo "<td>" . nl2br($applicant->getRemarks()) . "</td>";
+            echo "<td>" . $applicant->getStatusId() . "</td>";
+            echo "<td>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "MAILED: " . $applicant->getMailedAt() . "</br>";
+            echo "VERIFIED: " . $applicant->getConfirmedAt() . "</br>";
+            echo "PAYMENTMAILED: " . $applicant->getPaymentRequestedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "CREATED: " . $applicant->getCreatedAt() . "</br>";
+            echo "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table></div>";
 
-} else {
-    echo "You need to edit your database-related parts of the configuration in order to properly connect to the database.";
-}
+        } else {
+            echo "<p>You need to edit your database-related parts of the configuration in order to properly connect to the database.</p>";
+        }
 
+        ?>
+        </p>
+    </div><!-- /.starter-template -->
+</div><!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="../../js/bootstrap-formhelpers.min.js"></script>
+</body>
+</html>
