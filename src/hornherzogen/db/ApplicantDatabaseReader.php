7b563732aeb889d09e39c16982955b82211a32a7
Hugo Hirsch <github@hugo-hirsch.de>
2017-03-27 00:14:23 +0200

    Issue #66: Setup.

--- src/hornherzogen/db/ApplicantDatabaseReader.php
+++ src/hornherzogen/db/ApplicantDatabaseReader.php
@@ -5,5 +5,23 @@ namespace hornherzogen\db;
 
 class ApplicantDatabaseReader extends BaseDatabaseWriter
 {
+    public function listByFoodCategoryPerWeek($week)
+    {
+        return NULL;
+    }
 
+    public function listByRoomCategoryPerWeek($week)
+    {
+        // array of array
+        // 1 -> all single rooms
+        // 2 -> all double rooms
+        // 3 -> all triple rooms
+        // 4 -> all other rooms
+        return array(
+            '1' => array(),
+            '2' => array(),
+            '3' => array(),
+            '4' => array(),
+        );
+    }
 }
