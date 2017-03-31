d4e347dd59540f27a6f695cc9619279fba43d878
Hugo Hirsch <github@hugo-hirsch.de>
2017-03-31 22:07:23 +0200

    Issue #69: Fix db_rooms.php

--- admin/db/db_rooms.php
+++ admin/db/db_rooms.php
@@ -1,26 +1,140 @@
+<!DOCTYPE html>
 <?php
 require '../../vendor/autoload.php';
 
-use \hornherzogen\ConfigurationWrapper;
+use hornherzogen\AdminHelper;
+use hornherzogen\ConfigurationWrapper;
+use hornherzogen\db\RoomDatabaseReader;
+use hornherzogen\FormHelper;
+use hornherzogen\HornLocalizer;
 
-echo "<h1>Retrieving rooms from DB ....</h1>";
+$adminHelper = new AdminHelper();
+$localizer = new HornLocalizer();
+$formHelper = new FormHelper();
 $config = new ConfigurationWrapper();
+$roomReader = new RoomDatabaseReader();
 
-if ($config->isValidDatabaseConfig()) {
+?>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Herzogenhorn 2017 Anmeldung Raumübersicht">
+    <meta name="author" content="OTG">
+    <meta name="robots" content="none,noarchive,nosnippet,noimageindex"/>
+    <link rel="icon" href="../../favicon.ico">
 
-    try {
-        $db = new PDO('mysql:host=' . $config->dbhost() . ';dbname=' . $config->dbname(), $config->dbuser(), $config->dbpassword());
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    <title>Herzogenhorn Adminbereich - Raumliste</title>
 
-        $q = $db->query("SELECT r.name, r.capacity FROM `rooms` r");
-        while ($row = $q->fetch()) {
-            print "<h2>'$row[name]' has place for $row[capacity] people</h2>\n";
-        }
-    } catch (PDOException $e) {
-        print "Unable to connect to db:" . $e->getMessage();
-    }
+    <!-- Bootstrap core CSS -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../../css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-} else {
-    echo "You need to edit your database-related parts of the configuration in order to properly connect to the database.";
-}
+    <!-- Custom styles for this template -->
+    <link href="../../css/starter-template.css" rel="stylesheet">
+    <link href="../../css/theme.css" rel="stylesheet">
 
+    <!-- Calendar-related stuff -->
+    <link href="../../css/bootstrap-formhelpers.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../index.php"><span class="glyphicon glyphicon-tree-conifer"></span>
+                <?php echo $localizer->i18n('MENU.MAIN'); ?></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="../"><span
+                                class="glyphicon glyphicon-briefcase"></span> <?php echo $localizer->i18n('MENU.ADMIN'); ?>
+                    </a>
+                </li>
+                <?php $adminHelper->showSuperUserMenu(); ?>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><?php echo $adminHelper->showUserLoggedIn(); ?></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+</nav>
+
+<div class="container theme-showcase">
+    <div class="starter-template">
+        <a href="https://github.com/ottlinger/hornherzogen" target="_blank"><img
+                    style="position: absolute; top: 100px; right: 0; border: 0;"
+                    src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67"
+                    alt="Fork me on GitHub"
+                    data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+
+        <h1>
+            <span class="glyphicon glyphicon-sunglasses"></span> verfügbare Räume auf dem Horn
+        </h1>
+
+        <p>
+            <?php
+            if ($config->isValidDatabaseConfig()) {
+
+                $rooms = $roomReader->listRooms();
+
+                echo '<div class="table-responsive"><table class="table table-striped">';
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>Name</th>";
+                echo "<th>Kapazität</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+
+                foreach ($rooms as $room) {
+                    echo "<tr>";
+                    echo "<td>" . $room[name] . "</td>";
+                    echo "<td>" . $room[capacity] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</tbody>";
+                echo "</table></div>";
+
+            } else {
+                echo "<p>You need to edit your database-related parts of the configuration in order to properly connect to the database.</p>";
+            }
+            ?>
+    </div><!-- /.starter-template -->
+</div><!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="../../js/bootstrap-formhelpers.min.js"></script>
+</body>
+</html>
--- src/hornherzogen/db/RoomDatabaseReader.php
+++ src/hornherzogen/db/RoomDatabaseReader.php
@@ -6,15 +6,34 @@ namespace hornherzogen\db;
 class RoomDatabaseReader extends BaseDatabaseWriter
 {
     /**
+     * Retrieve all rooms with their capacity.
+     * @return array a simple list of rooms to show in the UI
+     */
+    public function listRooms()
+    {
+        $results = array();
+        if (self::isHealthy()) {
+            $dbResult = $this->database->query("SELECT r.name, r.capacity from `rooms` r ORDER BY r.name");
+            if (false === $dbResult) {
+                $error = $this->database->errorInfo();
+                print "DB-Error\nSQLError=$error[0]\nDBError=$error[1]\nMessage=$error[2]";
+            }
+            while ($row = $dbResult->fetch()) {
+                //  access all members print "<h2>'$row[name]' has place for $row[capacity] people</h2>\n";
+                $results[] = $row;
+            }
+        }
+        return $results;
+    }
+
+    /**
      * Retrieve all rooms with their capacity and booking by week.
      *
      * @param $week week choice, null for both weeks.
      * @return array a simple list of rooms to show in the UI
      */
-    public function listRooms($week)
+    public function listRoomBookings($week)
     {
-        echo "<h4>healthy</h4>";
-
         $results = array();
         if (self::isHealthy()) {
 
