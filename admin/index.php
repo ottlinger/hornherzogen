c3b327aad822b55e23d300fc9fad3fc4b8c59598
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-03 23:42:27 +0200

    Issue #74: add fake graphs

--- admin/index.php
+++ admin/index.php
@@ -111,7 +111,11 @@ $localizer = new HornLocalizer();
             Herzogenhornanmeldungen des Jahres <?php echo $localizer->i18n('CONST.YEAR'); ?>
             <h2 class="lead">Oben auswählen, was man machen will.</h2>
         </h1>
-
+        <hr />
+        <h2>Woche 1</h2>
+        <div id="piechart_week1" style="width: 600px; height: 500px;"></div>
+        <h2>Woche 2</h2>
+        <div id="piechart_week2" style="width: 600px; height: 500px;"></div>
     </div><!-- /.starter-template -->
 </div><!-- /.container -->
 
@@ -124,5 +128,31 @@ $localizer = new HornLocalizer();
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
 <script src="../js/bootstrap-formhelpers.min.js"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    google.charts.load("current", {packages: ["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Task', 'Hours per Day'],
+            ['Work', 11],
+            ['Eat', 2],
+            ['Commute', 2],
+            ['Watch TV', 2],
+            ['Sleep', 7]
+        ]);
+
+        var options = {
+            title: 'My Daily Activities',
+            is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_week1'));
+        chart.draw(data, options);
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_week2'));
+        chart.draw(data, options);
+    }
+</script>
 </body>
 </html>
