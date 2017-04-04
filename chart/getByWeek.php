368dd15ed039e6e52ba7ee4bb6f49e7219ff8183
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-04 23:50:42 +0200

    Issue #74: fix NPE.

--- chart/getByWeek.php
+++ chart/getByWeek.php
@@ -2,10 +2,12 @@
 require '../vendor/autoload.php';
 
 use hornherzogen\chart\ChartHelper;
+use hornherzogen\FormHelper;
 
 $chartHelper = new ChartHelper();
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['week'])) {
+    $formHelper = new FormHelper();
     $week = $formHelper->filterUserInput($_GET['week']);
     echo $chartHelper->getByWeek($week);
 }
