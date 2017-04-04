240120c665bed736fd3a0fdaf244d5440fba2c51
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-04 10:18:18 +0200

    Issue #74: add backend service

--- chart/getByWeek.php
+++ chart/getByWeek.php
@@ -1,3 +1,12 @@
 <?php
+require '../vendor/autoload.php';
 
+use hornherzogen\chart\ChartHelper;
+
+$chartHelper = new ChartHelper();
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['week'])) {
+    $week = $formHelper->filterUserInput($_GET['week']);
+    echo $chartHelper->getByWeek($week);
+}
 ?>
