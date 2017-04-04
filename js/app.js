b57812b2f9792c4564269cbdbc12313ae159cc2f
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-04 13:28:48 +0200

    Fix codacy warning about variable name

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(function () {
         $("#room").change(
             function () {
-                var hh_firstPersonSelector = $("#together1-group");
-                var hh_secondPersonSelector = $("#together2-group");
+                var firstPersonSelector = $("#together1-group");
+                var secondPersonSelector = $("#together2-group");
 
-                hh_firstPersonSelector.show();
-                hh_secondPersonSelector.show();
+                firstPersonSelector.show();
+                secondPersonSelector.show();
 
                 if (this.selectedIndex === 2) {
-                    hh_firstPersonSelector.hide();
-                    hh_secondPersonSelector.hide();
+                    firstPersonSelector.hide();
+                    secondPersonSelector.hide();
                 }
 
                 if (this.selectedIndex === 1) {
-                    hh_secondPersonSelector.hide();
+                    secondPersonSelector.hide();
                 }
 
             });
