8ae7023086b2f95abd49236a22fbaf8e53e6f232
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-09 00:33:50 +0200

    Issue #78: bugfix usage in views

--- admin/db/dbtest.php
+++ admin/db/dbtest.php
@@ -2,6 +2,9 @@
 require '../../vendor/autoload.php';
 
 use hornherzogen\ConfigurationWrapper;
+use hornherzogen\db\DatabaseHelper;
+
+$databaseHelper = new DatabaseHelper();
 
 echo "<h1>Establishing DB connection to insert data ....</h1>";
 
@@ -15,19 +18,19 @@ if ($config->isValidDatabaseConfig()) {
 
         echo "<h2>Cleanup1: Remove all bookings ...</h2>";
         $result = $db->exec("DELETE FROM `roombooking` WHERE applicantId in ((SELECT a.id AS applicantId FROM `applicants` a where a.vorname like '%Hugo%'))");
-        $this->databaseHelper->logDatabaseErrors($result, $db);
+        $databaseHelper->logDatabaseErrors($result, $db);
 
         echo "<h2>Cleanup2: Remove all applicants ...</h2>";
         $result = $db->exec("DELETE FROM `applicants` WHERE vorname='Hugo'");
-        $this->databaseHelper->logDatabaseErrors($result, $db);
+        $databaseHelper->logDatabaseErrors($result, $db);
 
         echo "<h2>Insert fake applicant ...</h2>";
         $result = $db->exec("INSERT INTO `applicants` (language,vorname,nachname,week,email,statusId) SELECT 'de','Hugo','Hirsch','week1','foo@bar.de',id from `status` WHERE NAME = 'APPLIED'");
-        $this->databaseHelper->logDatabaseErrors($result, $db);
+        $databaseHelper->logDatabaseErrors($result, $db);
 
         echo "<h2>Show all applicants ...</h2>";
         $q = $db->query("SELECT a.vorname, a.nachname, a.created, s.name FROM `applicants` a, `status` s WHERE s.id = a.statusId");
-        $this->databaseHelper->logDatabaseErrors($result, $db);
+        $databaseHelper->logDatabaseErrors($result, $db);
 
         while ($row = $q->fetch()) {
             print "<h2>$row[vorname] $row[nachname] created at $row[created] with status $row[name]</h2>\n";
@@ -35,7 +38,7 @@ if ($config->isValidDatabaseConfig()) {
 
         echo "<h2>Perform room booking ...</h2>";
         $result = $db->exec("INSERT INTO `roombooking` (roomId,applicantId) VALUES ( (SELECT r.id AS roomId FROM `rooms` r where r.name like '%Zimmer1%'), (SELECT a.id AS applicantId FROM `applicants` a where a.vorname like '%Hugo%') )");
-        $this->databaseHelper->logDatabaseErrors($result, $db);
+        $databaseHelper->logDatabaseErrors($result, $db);
 
     } catch (PDOException $e) {
         print "Unable to connect to db:" . $e->getMessage();
@@ -44,4 +47,3 @@ if ($config->isValidDatabaseConfig()) {
 } else {
     echo "You need to edit your database-related parts of the configuration in order to properly connect to the database.";
 }
-
