992ef9dc7fe20f2ab70ad6f83bcca240037cec97
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-10 22:47:23 +0200

    Issue #77: Prepare localized mail template

---
+++ src/hornherzogen/admin/FlexibilityMailGenerator.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace hornherzogen\admin;
+
+class FlexibilityMailGenerator 
+{
+    private $applicant;
+
+    function __construct($applicant)
+    {
+        $this->applicant = $applicant;
+    }
+
+    public function getSubject() {
+        
+    }
+
+    public function getBody() {
+
+    }
+
+
+}
---
+++ tests/admin/FlexibilityMailGeneratorTest.php
@@ -0,0 +1,52 @@
+<?php
+use hornherzogen\admin\FlexibilityMailGenerator;
+use PHPUnit\Framework\TestCase;
+
+use hornherzogen\Applicant;
+
+class FlexibilityMailGeneratorTest extends TestCase
+{
+    private $generator = null;
+    private $applicant = null;
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $this->applicant = new Applicant();
+        $this->generator = new FlexibilityMailGenerator($this->applicant);
+    }
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        $this->generator = null;
+        $this->applicant = null;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('hornherzogen\admin\FlexibilityMailGenerator', $this->generator);
+    }
+
+    public function testSubjectRetrieval()
+    {
+        $this->assertNotNull($this->generator);
+        $this->assertNull($this->generator->getSubject());
+    }
+
+    public function testBodyRetrieval()
+    {
+        $this->assertNotNull($this->generator);
+        $this->assertNull($this->generator->getBody());
+    }
+
+}
