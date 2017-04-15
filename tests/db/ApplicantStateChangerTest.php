24646bc2e19542f3d76caa7bc3a70a298fb3a650
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-15 13:13:18 +0200

    Issue #80: base infra for state changer.

---
+++ tests/db/ApplicantStateChangerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use hornherzogen\db\ApplicantStateChanger;
+use PHPUnit\Framework\TestCase;
+
+class ApplicantStateChangerTest extends TestCase
+{
+    private $stateChanger = null;
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $this->stateChanger = new ApplicantStateChanger();
+    }
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        $this->stateChanger = null;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('hornherzogen\db\ApplicantStateChanger', $this->stateChanger);
+    }
+
+    public function testWithoutConfigEmptyListIsRetrievedWithWeekParameter()
+    {
+        $this->assertFalse($this->stateChanger->changeStateTo("applicantId", "stateId"));
+    }
+
+}
