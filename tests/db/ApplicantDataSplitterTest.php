c492ce305d51d8f941a07755ec992faa4f9464e6
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-17 21:21:50 +0200

    Issue #79: Extract utility class.

---
+++ tests/db/ApplicantDataSplitterTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use hornherzogen\db\ApplicantDataSplitter;
+use PHPUnit\Framework\TestCase;
+
+class ApplicantDataSplitterTest extends TestCase
+{
+    private $stateChanger = null;
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $this->stateChanger = new ApplicantDataSplitter();
+    }
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        $this->stateChanger = null;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('hornherzogen\db\ApplicantDataSplitter', $this->stateChanger);
+    }
+
+}
