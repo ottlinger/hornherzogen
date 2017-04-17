289532bd9ff567ad9d35b378a8e8e56976ac2798
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-17 21:31:06 +0200

    Issue #79: extract function

--- src/hornherzogen/db/ApplicantDataSplitter.php
+++ src/hornherzogen/db/ApplicantDataSplitter.php
@@ -1,9 +1,55 @@
 <?php
 declare(strict_types=1);
-namespace hornherzogen\db;
 
+namespace hornherzogen\db;
 
 class ApplicantDataSplitter
 {
+    private $databaseHelper;
+
+    function __construct()
+    {
+        $this->databaseHelper = new DatabaseHelper();
+    }
+
+
+    public function splitByRoomCategory($applicantsResultFromDatabase)
+    {
+        $results = array(
+            '1' => array(),
+            '2' => array(),
+            '3' => array(),
+            '4' => array(),
+        );
+
+        if (NULL != $applicantsResultFromDatabase) {
+
+            while ($row = $applicantsResultFromDatabase->fetch()) {
+                $applicant = $this->databaseHelper->fromDatabaseToObject($row);
+
+                switch ($applicant->getRoom()) {
+                    case "1bed":
+                        $results['1'][] = $applicant;
+                        break;
+
+                    case "2bed":
+                        $results['2'][] = $applicant;
+                        break;
+
+                    case "3bed":
+                        $results['3'][] = $applicant;
+                        break;
+
+                    default:
+                        $results['4'][] = $applicant;
+                        break;
+                }
+            }
+        }
+
+        return $results;
+
+    }
+
 
 }
