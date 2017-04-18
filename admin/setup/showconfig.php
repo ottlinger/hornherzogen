499122b7d840ee3db06e92bda968b64f32f2d667
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-18 23:41:12 +0200

    Issue #91: Add superadmin menu to show current account config

--- admin/setup/showconfig.php
+++ admin/setup/showconfig.php
@@ -2,7 +2,11 @@
 
 require '../../vendor/autoload.php';
 
-use \hornherzogen\ConfigurationWrapper;
+use hornherzogen\ConfigurationWrapper;
+use hornherzogen\admin\BankingConfiguration;
 
 echo "<h2>What's the current configuration?</h2>";
 echo new ConfigurationWrapper();
+
+echo "<h2>What's the account configuration?</h2>";
+echo new BankingConfiguration();
