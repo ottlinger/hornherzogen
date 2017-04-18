ea16664e14348a9fc636c869544b4c3995f129e9
Hugo Hirsch <github@hugo-hirsch.de>
2017-04-18 18:54:36 +0200

    Issue #79: more tests.

---
+++ tests/db/BookingErrorCheckerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use hornherzogen\db\BookingErrorChecker;
+use PHPUnit\Framework\TestCase;
+
+class BookingErrorCheckerTest extends TestCase
+{
+    private $errorChecker = null;
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $this->errorChecker = new BookingErrorChecker();
+    }
+
+    /**
+     * Teardown the test environment.
+     */
+    public function tearDown()
+    {
+        $this->errorChecker = null;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @test
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('hornherzogen\db\BookingErrorChecker', $this->errorChecker);
+    }
+
+}
