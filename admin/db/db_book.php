b98e1cb908637cf676bc40b50f67c68ef1402914
Hugo Hirsch <github@hugo-hirsch.de>
2017-05-03 00:56:44 +0200

    Issue #96: Remove verifying rooms while booking

--- admin/db/db_book.php
+++ admin/db/db_book.php
@@ -244,13 +244,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['week'])) {
 
         foreach ($_POST['applicantId'] as $submittedApplicantId) {
             $iid = $formHelper->filterUserInput($_POST['id']);
-
-            if ($roomWriter->canRoomBeBooked($iid)) {
-                $persistId = $roomWriter->performBooking($iid, $submittedApplicantId);
-                if (NULL != $persistId) {
-                    echo "<p style=\"color: darkgreen;\"><span class=\"glyphicon glyphicon-send\"></span> Buchung angelegt mit id #" . $persistId . " für Person mit Id #" . $submittedApplicantId . "</p>";
-                    $_POST['applicantId'] = NULL;
-                }
+            // WEEK is not taken care of
+            //if ($roomWriter->canRoomBeBooked($iid)) {
+            $persistId = $roomWriter->performBooking($iid, $submittedApplicantId);
+            if (NULL != $persistId) {
+                echo "<p style=\"color: darkgreen;\"><span class=\"glyphicon glyphicon-send\"></span> Buchung angelegt mit id #" . $persistId . " für Person mit Id #" . $submittedApplicantId . "</p>";
+                $_POST['applicantId'] = NULL;
+                //  }
             } else {
                 echo "<p style=\"color: red;\"><span class=\"glyphicon glyphicon-bell\"></span> Kann Raum #" . $iid . " für Person #" . $submittedApplicantId . " nicht buchen, da Raum sonst überbucht würde.";
             }
